9cb33e0499d3530295016257ed12b71f58b29e0f
Mukhopadhyay <praneshmukherjee7@gmail.com>
2023-01-13 04:25:16 +0530

    Removing notebook + less wait time on start.sh

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -3,6 +3,5 @@ from api.healthcheck import router as healthcheck_router
 from api.utils import router as utility_router
 
 router = APIRouter()
-router.include_router(healthcheck_router, prefix='/healthcheck', tags="healthchecks")
-router.include_router(utility_router, prefix='/utils', tags="utility")
-
+router.include_router(healthcheck_router, prefix='/healthcheck', tags=["healthchecks"])
+router.include_router(utility_router, prefix='/utils', tags=["utility"])
--- start.sh
+++ start.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-# Wait for 10 seconds
-sleep 10
-echo "Done waiting!"
+# Caveman wait for DB to get started
+sleep 5
 
 # Database migration
 alembic revision -m "migration-script" --autogenerate
