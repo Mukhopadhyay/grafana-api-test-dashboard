99a418aa25bb239a8da361b8c57c368a7efa376a
Mukhopadhyay <praneshmukherjee7@gmail.com>
2023-01-16 22:20:28 +0530

    Removing unused ORM classes

--- src/api/data.py
+++ src/api/data.py
@@ -7,35 +7,16 @@ from schemas.db import apis as api_schemas
 router = APIRouter()
 
 @router.get("/")
-def index():
+async def index():
     return {"msg": "data endpoint"}
 
 @router.post("/endpoint")
-def insert_endpoint_result(endpoint: api_schemas.Endpoint):
+async def insert_endpoint_result(endpoint: api_schemas.Endpoint):
     data = apis.Endpoint( **endpoint.dict() )
     return utils.insert_into_endpoint(data)
-    # with database.get_session() as session:
-    #     with session.begin():
-    #         try:
-    #             session.add(endpoint)
-    #         except Exception as err:
-    #             print(err)
-    #             session.rollback()
-    #             return {"msg": "Couldn't insert into database"}
-    #         else:
-    #             session.commit()
-    #             return {"msg": "Data inserted"}
+
 
 @router.delete("/endpoint")
-def truncate_endpoint_results_table():
+async def truncate_endpoint_results_table():
     return utils.truncate_endpoint_tbl()
-    # with database.get_session() as session:
-    #     with session.begin():
-    #         try:
-    #             session.execute('TRUNCATE TABLE endpoint;')
-    #         except Exception as err:
-    #             session.rollback()
-    #             return {"msg": "Couldn't truncate table"}
-    #         else:
-    #             session.commit()
-    #             return {"msg": "Table truncated"}
+
